08edfe77c1ef1baa532129989f8abea853fc86e7
Hans Foster <fosterhans@gmail.com>
2019-11-21 10:54:26 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,7 +2,8 @@ class Student
   attr_accessor :id, :name, :grade
 
   def self.new_from_db(row)
-    new_student = Student.new()
+    new_student = Student.new
+    new_student.id = row[]
     # create a new Student object given a row from the database
   end
 
